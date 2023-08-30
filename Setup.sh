f2f9f65aac038b38a61f02ce94b0d78ec4d1eb44
Stormy <80485413+Kinda-Stormy@users.noreply.github.com>
2023-08-30 09:47:11 -0700

    Added Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,26 @@
+# Installing Flatpak
+
+echo "Starting Flatpak Installation (May require password for sudo)"
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Installing all apps I use
+
+echo "Starting Application Installation"
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub io.lmms.LMMS
+flatpak install flathub org.videolan.VLC
+flatpak install flathub com.usebottles.bottles
+flatpak install flathub com.visualstudio.code
+
+# Installing KDE
+
+echo "Starting kde-plasma-desktop Installation (May require password for sudo)"
+sudo apt update
+sudo apt install kde-plasma-desktop
+
+# Finshing things
+
+echo "Everything has finished installing!"
